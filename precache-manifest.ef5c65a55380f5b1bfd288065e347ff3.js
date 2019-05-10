1786392882e0736dddde7ae4f475f6ffab2c4fb6
Isidor Nygren <karlisidornygren@gmail.com>
2019-05-10 14:51:29 +0100

    Updates

---
+++ precache-manifest.ef5c65a55380f5b1bfd288065e347ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7991540550c884205bb3e119f424817",
+    "url": "/missommar-2019/index.html"
+  },
+  {
+    "revision": "6602cd6a258349d8a605",
+    "url": "/missommar-2019/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/missommar-2019/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6602cd6a258349d8a605",
+    "url": "/missommar-2019/static/js/main.6f678413.chunk.js"
+  },
+  {
+    "revision": "99169fce011ed7d48514",
+    "url": "/missommar-2019/static/js/runtime~main.7c87d2d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/missommar-2019/static/media/logo.5d5d9eef.svg"
+  }
+]);
