8160de16da3929ba603a3fc87b740f1e8b5a96e6
Isidor Nygren <karlisidornygren@gmail.com>
2019-05-10 17:10:40 +0100

    Added basic sites

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./navbar";
+import Info from "./info";
+import Filmer from "./filmer";
+import Kontakt from "./kontakt";
+import CountDown from "./countdown";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <CountDown />
+      <Info />
+      <Filmer />
+      <Kontakt />
     </div>
   );
 }
---
+++ src/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class NavBar extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Missömmar 2019</h1>
+      </div>
+    );
+  }
+}
